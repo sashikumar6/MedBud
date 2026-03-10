a5962c275952faa8e1b0baa1d933a96de3141dd1
Sashikumar Nehrumohan <sashikumarnehrumohan@Mac.hsd1.ca.comcast.net>
2026-03-10 11:24:30 -0700

    folders created

---
+++ template.sh
@@ -0,0 +1,16 @@
+#Directory creation
+mkdir -p src
+mkdir -p research
+
+#file creation
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirement.txt
+
+
+echo "creation succeed"
